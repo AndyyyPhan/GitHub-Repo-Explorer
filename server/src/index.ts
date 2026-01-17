0970ae6e03258681a9734a0ee74b9301d9b70fb8
Andy Phan <andyyyphan@gmail.com>
2026-01-17 16:04:33 -0500

    Switch app order so that cors is at the top of the file

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,14 +7,14 @@ import { authRouter } from "./routes/auth.js";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json());
-
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
+
+app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/me", meRouter);
